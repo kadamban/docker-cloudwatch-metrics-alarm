c5d1c9de9243b96dd1cca26b19a6a63b51d717a3
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2017-12-22 15:50:24 +0530

    Adding Ok notifcation

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,23 +6,25 @@ sed -r -i  "s#AWS_SECRET_ACCESS_KEY#$AWS_SECRET_ACCESS_KEY#g" /root/.aws/credent
 
 sed -r -i "s/AWS_AVAILABILITY_ZONE/$AWS_AVAILABILITY_ZONE/g" /root/.aws/credentials
 
+
+
 instance=$(curl -XGET  http://169.254.169.254/latest/meta-data/instance-id)
 
   aws cloudwatch put-metric-alarm --alarm-name "$NODE_ENV""-High-Disk-Utilization-""$instance" \
   --metric-name DiskSpaceUtilization --namespace System/Linux \
   --statistic Average --period 300 --threshold 70 --comparison-operator GreaterThanThreshold \
   --dimensions Name=InstanceId,Value=$instance Name=Filesystem,Value=overlay Name=MountPath,Value=/ \
-  --evaluation-periods 1 --alarm-actions $ARN
+  --evaluation-periods 1 --alarm-actions $ARN --ok-actions $ARN
 
   aws cloudwatch put-metric-alarm --alarm-name "$NODE_ENV""-High-CPU-Utilization-""$instance" \
   --metric-name CPUUtilization --namespace AWS/EC2 \
   --statistic Average --period 300 --threshold 80 --comparison-operator GreaterThanThreshold \
-  --dimensions Name=InstanceId,Value=$instance   --evaluation-periods 1 --alarm-actions $ARN
+  --dimensions Name=InstanceId,Value=$instance   --evaluation-periods 1 --alarm-actions $ARN --ok-actions $ARN
 
   aws cloudwatch put-metric-alarm --alarm-name "$NODE_ENV""-High-Memory-Utilization-""$instance" \
   --metric-name MemoryUtilization --namespace System/Linux \
   --statistic Average --period 300 --threshold 75 --comparison-operator GreaterThanThreshold \
-  --dimensions Name=InstanceId,Value=$instance   --evaluation-periods 1 --alarm-actions $ARN
+  --dimensions Name=InstanceId,Value=$instance   --evaluation-periods 1 --alarm-actions $ARN --ok-actions $ARN
 
 while true; do
 
